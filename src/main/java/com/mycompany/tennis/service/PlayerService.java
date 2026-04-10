161304644bc4f8db725ee788bb21c6b6dba45f4c
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2026-04-11 00:22:26 +0200

    Exercice 5 : Créer un Tournoi avec Hibernate

--- src/main/java/com/mycompany/tennis/service/PlayerService.java
+++ src/main/java/com/mycompany/tennis/service/PlayerService.java
@@ -1,7 +1,10 @@
 package com.mycompany.tennis.service;
 
+import com.mycompany.tennis.HibernateUtil;
 import com.mycompany.tennis.entity.Player;
 import com.mycompany.tennis.repository.PlayerRepositoryImpl;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class PlayerService {
 
@@ -17,5 +20,57 @@ public class PlayerService {
 
     public Player getPlayer(Long id) {
         return playerRepository.getById(id);
+    }
+
+    public void updatePlayerLastname(Long id, String newLastname) {
+        Session session = null;
+        Transaction tx = null;
+        Player player = getPlayer(id);
+
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+
+            player.setLastname(newLastname);
+            Player player2 = (Player)session.merge(player);
+            session.merge(player);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void updatePlayerSex(Long id, char newSex) {
+        Session session = null;
+        Transaction tx = null;
+        Player player = getPlayer(id);
+
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+
+            player.setSex(newSex);
+            Player player2 = (Player)session.merge(player);
+            session.merge(player);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
